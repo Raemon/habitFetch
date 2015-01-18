83b86c75868f550f218bd3064292f73050959cc2
Raemon <raemon777@gmail.com>
2015-01-17 22:09:43 -0500

    Cleaned up unnecessary print statements

--- habitFetch.py
+++ habitFetch.py
@@ -2,6 +2,7 @@ import requests
 import sqlite3
 import json
 import time
+import settings
 
 db = sqlite3.connect("habitRPG_data.sql") #you can put whatever ... created if not exist
 conn = db.cursor()
@@ -11,9 +12,6 @@ Notes
 If you do multiple habits in a short timespan, 
     it saves the old timestamp and updates it with new graph data
 '''
-
-user_id = "0666e772-0e7b-43fb-97c0-e58eabd62042"
-api_key = "7cd952c6-4a13-42c8-afd6-3898aca55b4f"
 
 class HabitApi(object):
     DIRECTION_UP = "up"
@@ -74,55 +72,21 @@ class HabitApi(object):
         return self.request("post", url, data=data, headers=headers).json()
 
 
-hrpg_access = HabitApi(user_id = "0666e772-0e7b-43fb-97c0-e58eabd62042", api_key = "7cd952c6-4a13-42c8-afd6-3898aca55b4f")
+hrpg_access = HabitApi(user_id = settings.user_id, api_key = settings.api_key)
 
-# # Display all user attributes
-# for x in hrpg_access.user():
-# 	print x
-# print ""
-
-# # Display all tasks attributes
-# for x in hrpg_access.tasks():
-#     print x['text']
-# print ""
-
-for x in hrpg_access.tasks()[0]:
-    print x
-
-conn.execute("DROP TABLE Tasks")
 conn.execute("CREATE TABLE IF NOT EXISTS Tasks (name text, task_type text, timestamp int, value float);")
 
 def add_task(name, task_id, task_type, timestamp, value):
     conn.execute("SELECT * FROM Tasks WHERE name == ? AND timestamp == ? AND task_type == ? AND value == ?", (name, timestamp, task_type, value))
     if len(conn.fetchall()) == 0:
-        if type(timestamp) == float:
-            if type(value) == float or type(value) == int:
-                if type(name) == str:
-                    if type(task_id) == str:
-                        if type(task_type) == str:
-                            print "adding task"
-                            conn.execute("INSERT INTO Tasks (name, task_type, timestamp, value) VALUES (?,?,?,?)",(name, task_type, timestamp, value))
-                            db.commit()
-                            conn.execute("SELECT * FROM Tasks WHERE name == ? AND timestamp == ? AND task_type == ?", (name, timestamp, task_type))
-                            print "Number of Identical Tasks", len(conn.fetchall())
-
-                        else:
-                            raise Exception("task_type is not a string")
-                    else:
-                        raise Exception("task_type is not a string")
-                else:
-                    raise Exception("name_type is not a string")
-            else:
-                raise Exception("value type is not a float", value)
-        else:
-            raise Exception("timestamp type is not a float: ", timestamp)
+        print "adding task"
+        conn.execute("INSERT INTO Tasks (name, task_type, timestamp, value) VALUES (?,?,?,?)",(name, task_type, timestamp, value))
+        db.commit()
+        conn.execute("SELECT * FROM Tasks WHERE name == ? AND timestamp == ? AND task_type == ?", (name, timestamp, task_type))
+        print "Number of Identical Tasks", len(conn.fetchall())
     else:
-        raise Exception("task history already exists")
+        print "task history already exists"
 
-
-def check_activity(name, type, timestamp, value):
-    conn.execute("SELECT * FROM Tasks WHERE timestamp > ? AND timestamp < ?",(start_time,end_time))
-    return conn.fetchall()
 
 # def GetAllActivitiesOnDate(month,day,year):
 #     start_time = time.mktime((year,month,day,0,0,0,0,0,0)) 
@@ -131,47 +95,24 @@ def check_activity(name, type, timestamp, value):
 #     return conn.fetchall()
 
 
+def convert_date(old_timestamp):
+    try:
+        new_timestamp = float(old_timestamp)/1000
+    except:
+        new_timestamp = time.strptime(old_timestamp.split(".")[0], "%Y-%m-%dT%H:%M:%S")
+        new_timestamp = time.mktime(new_timestamp)
+    return new_timestamp
+
 for habit in hrpg_access.user()['habits']:
-    print habit['text']
-    print habit['id']
     for date in habit['history']:
-        old_timestamp = date['date']
-        try:
-            new_timestamp = float(old_timestamp)/1000
-        except:
-            new_timestamp = time.strptime(old_timestamp.split(".")[0], "%Y-%m-%dT%H:%M:%S")
-            new_timestamp = time.mktime(new_timestamp)
-        add_task(str(habit['text']), str(habit['id']), "habit", new_timestamp, date['value'])
+        add_task(str(habit['text']), str(habit['id']), "habit", convert_date(date['date']), date['value'])
 
-
-    print ""
-
+for daily in hrpg_access.user()['dailys']:
+    for date in daily['history']:
+        add_task(str(daily['text']), str(daily['id']), "daily", convert_date(date['date']), date['value'])  
 
 data = conn.execute("SELECT * FROM Tasks")
 for x in data:
     print x
-# newlist = sorted(data, key=lambda k: k['name']) 
-
-# for x in newlist:
-#     print x
-
-
-
-# for habit in h.user()['habits']:
-# 	for x in habit:
-# 		print x
-# 		print habit[x]
-# 		print ""
-# 	print ""
-
-# 	for x in habit['history']:
-# 		print x
-# 	print ""
-
-# for task in h.tasks():
-# 	print task['history']
-# 	for item in task:
-# 		print item
-# 	print ""
 
 
---
+++ settings.py
@@ -0,0 +1,2 @@
+user_id = "0666e772-0e7b-43fb-97c0-e58eabd62042"
+api_key = "7cd952c6-4a13-42c8-afd6-3898aca55b4f"
